c83914569b557dad9fc6a0a89091c62caaa5982e
AgandeMajesty <agandemajesty@gmail.com>
2025-08-05 15:55:47 +0100

    Update before EC2 deploy

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-r1u9pa*ndn&j&^lsm8+9t$=me)-h2j)#&#*&$0ap8aq=eu=s6g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['16.170.171.149', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
