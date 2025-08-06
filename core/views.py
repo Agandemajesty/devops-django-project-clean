0f007442798e8f2c0e831cd19b31ff8461fd937f
AgandeMajesty <agandemajesty@gmail.com>
2025-08-06 14:55:53 +0100

    Added basic home view and updated URL config

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to your Django App!")
+
 
 # Create your views here.
